d1d42a0d1303427582807fff2af0d1dc83632f9b
KavishDhiman <kavishdhiman2003@gmail.com>
2026-03-11 13:01:25 +0530

    UC5: Hello API using PUT PathVariable and QueryParam

--- hello-rest-api/src/main/java/com/bridgelabz/hello/controller/HelloController.java
+++ hello-rest-api/src/main/java/com/bridgelabz/hello/controller/HelloController.java
@@ -29,4 +29,10 @@ public class HelloController {
     public String helloPost(@RequestBody UserDTO user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
+    //UC5
+    @PutMapping("/hello/put/{firstName}")
+    public String helloPut(@PathVariable String firstName,
+                           @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
 }
