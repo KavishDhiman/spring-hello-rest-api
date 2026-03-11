cf35d666f2d25865741848d5b79165e9185e1385
KavishDhiman <kavishdhiman2003@gmail.com>
2026-03-11 12:43:31 +0530

    UC2: Hello API using Query Parameter

--- hello-rest-api/src/main/java/com/bridgelabz/hello/controller/HelloController.java
+++ hello-rest-api/src/main/java/com/bridgelabz/hello/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.bridgelabz.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    // UC1
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    // UC2
+    @GetMapping("/hello/query")
+    public String helloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
